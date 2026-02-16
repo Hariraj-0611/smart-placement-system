56de576aa0c6743c8067f5544e6f92225b461dad
Hariraj-0611 <rajhariraj78@gmail.com>
2026-02-16 15:52:55 +0530

    fix-db

--- placement-system/backend/backend/settings.py
+++ placement-system/backend/backend/settings.py
@@ -78,16 +78,8 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'crossover.proxy.rlwy.net',
-        'PORT': '35882',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
