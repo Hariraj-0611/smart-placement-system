bde3da8ff30f3500cf172e0c4f3ac604beec01a5
Hariraj-0611 <rajhariraj78@gmail.com>
2026-02-17 07:57:36 +0530

    txt

--- placement-system/backend/api/urls.py
+++ placement-system/backend/api/urls.py
@@ -1,21 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from . import views
-
-# router = DefaultRouter()
-# router.register(r'students', views.StudentProfileViewSet)
-# router.register(r'drives', views.CompanyDriveViewSet)
-# router.register(r'applications', views.ApplicationViewSet)
-
-# urlpatterns = [
-#     path('register/', views.register_student, name='register'),
-#     path('login/', views.login_user, name='login'),
-#     path('me/', views.get_current_user, name='current-user'),
-#     path('dashboard/student/', views.student_dashboard, name='student-dashboard'),
-#     path('dashboard/officer/', views.officer_dashboard, name='officer-dashboard'),
-#     path('', include(router.urls)),
-# ]
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
@@ -23,15 +5,13 @@ from . import views
 router = DefaultRouter()
 router.register(r'students', views.StudentProfileViewSet)
 router.register(r'drives', views.CompanyDriveViewSet)
-router.register(r'applications', views.ApplicationViewSet)  # This includes my-applications
+router.register(r'applications', views.ApplicationViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-     path('register/', views.register_student, name='register'),
+    path('register/', views.register_student, name='register'),
     path('login/', views.login_user, name='login'),
     path('me/', views.get_current_user, name='current-user'),
     path('dashboard/student/', views.student_dashboard, name='student-dashboard'),
     path('dashboard/officer/', views.officer_dashboard, name='officer-dashboard'),
     path('', include(router.urls)),
-    # ... other urls
 ]
